c5f4c5227cc58c781733b659dfb7c2e007c51983
Thorsten Liese <thorsten.liese@dieboldnixdorf.com>
2018-06-11 21:59:58 +0200

    BCSDM-5: moved to dn-sol.net

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dncashio.apili.xyz/dnapi/cash/v1/',
+  apiUrl: 'https://dncashapi.dn-sol.net/dnapi/cash/v1/',
   mqttUrl: 'wss://test.mosquitto.org:8081'
 };
