2d2dc9d1d3148dddc0eb92d89231714de5e68b2f
Thorsten Liese <thorsten.liese@dieboldnixdorf.com>
2018-11-10 14:08:12 +0100

    symbioticon setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dncashapi.dn-sol.net/dnapi/cash/v1/',
+  apiUrl: 'https://api.dncash.io/dnapi/cash/v1/',
   mqttUrl: 'wss://mosquitto.dn-sol.net'
 };
